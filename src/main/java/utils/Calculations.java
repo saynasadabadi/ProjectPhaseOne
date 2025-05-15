355bc69221cd5989491e9da69cd94734356cfbba
saynasadabadi <saynasadabadiiii@gmail.com>
2025-05-15 09:43:26 +0330

    added utils

---
+++ src/main/java/utils/Calculations.java
@@ -0,0 +1,9 @@
+package utils;
+
+import model.Point;
+
+public class Calculations {
+    public static Double calculateDistance(Point point1, Point point2){
+        return Math.sqrt(Math.pow(point1.getX() - point2.getX(), 2) + Math.pow(point1.getY() - point2.getY(), 2));
+    }
+}
