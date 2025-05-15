188f7d00511146844d538ffaa36291999dbdd2d2
saynasadabadi <saynasadabadiiii@gmail.com>
2025-05-15 09:59:14 +0330

    gave the Enum for PacketAndPortShape more details

--- src/main/java/model/PacketAndPortShape.java
+++ src/main/java/model/PacketAndPortShape.java
@@ -1,6 +1,24 @@
 package model;
 
+import java.awt.Color; // Import Color class
+
 public enum PacketAndPortShape {
-    SQUARE,
-    TRIANGLE,
+    SQUARE(4, Color.BLUE),
+    TRIANGLE(3, Color.RED);
+
+    private final int numberOfSides;
+    private final Color color;
+
+    private PacketAndPortShape(int numberOfSides, Color color) {
+        this.numberOfSides = numberOfSides;
+        this.color = color;
+    }
+
+    public int getNumberOfSides() {
+        return numberOfSides;
+    }
+
+    public Color getColor() {
+        return color;
+    }
 }
